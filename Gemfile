809df411bc2fb4936fe531118de73b226708c0ba
volmer <lrvolmer@gmail.com>
2013-03-15 00:04:30 -0300

    Rails new and dependencies

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'therubyracer', platforms: :ruby
+  gem 'less-rails'
+end
+
+gem 'jquery-rails'
+gem 'unicorn'
+gem 'responders'
+gem 'simple_form'
+gem 'twitter-bootstrap-rails'
+
+group :test, :development do
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Volmerius::Application.config.secret_token = 'f86d5ceef4a53a1eccd8075ff29bbc93f40f0f485b200ff16dbaca07878e428b6058ec65d6241ff079af1c33da1699374e81bd1fbd00491f91a6092f1fbd4ad0'

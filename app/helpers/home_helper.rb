1d7d445ee45d7bf7504c7dc6fd113cfa75b66537
Volmer <git@radicaos.com>
2013-04-19 12:49:04 -0300

    New layout

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def link_li(icon, text, href)
+    content_tag 'li' do
+      link_to href, target: '_blank' do
+        content_tag('i', ' ', class: icon) + ' ' + text
+      end
+    end
+  end
 end
